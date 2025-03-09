e58afcb16666741e50b01af71726d539797a7a9f
Bruno Daniel <bdao.profissional@gmail.com>
2025-03-08 21:52:57 -0300

    variaveis e operadores

---
+++ primeirosPassos/variaveisEOperadores.js
@@ -0,0 +1,24 @@
+// = atribuir valor
+// == atribuir e comparar valor
+// === atribuir, comparar e verificar tipo do valor
+
+// + somar
+// - subtrair
+// * multiplicar
+// / dividir
+// % resto da divisao
+
+console.log('Olá mundo!!');
+
+let mutavel = 'Pode alterar o valor';
+const imutavel = 'Não pode alterar o valor';
+
+let numMutavel = 8;
+numMutavel = 10;
+
+const numImutavel = 8;
+
+const somar = numMutavel + numImutavel; // 18
+const subtrair = numMutavel - numImutavel; // 2
+const multiplicar = numMutavel * numImutavel; // 80
+const dividir = numMutavel / numImutavel; // 1.25
