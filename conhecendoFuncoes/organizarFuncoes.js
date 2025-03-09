a01029077fd9838c9fe1a4e173aa6b75df6b966b
Bruno Daniel <bdao.profissional@gmail.com>
2025-03-08 22:18:34 -0300

    organizando funcoes

---
+++ conhecendoFuncoes/organizarFuncoes.js
@@ -0,0 +1,31 @@
+function main() {
+    console.log('------ Iniciando sistema ------');
+
+    const peso = 75;
+    const altura = 1.66;
+    const imc = calcularImc(peso, altura);
+    console.log(classificarImc(imc));
+
+    console.log('------ Finalizando sistema ------');
+};
+main(); // Sobrepeso
+
+function calcularImc( peso, altura ) {
+    return peso / Math.pow( altura, 2 );
+};
+
+function classificarImc( imc ) {
+    if ( imc <= 18.5 ) {
+        return 'Baixo peso';
+    } else if ( imc <= 24.9 ) {
+        return 'Intervalo normal';
+    } else if ( imc <= 29.9 ) {
+        return 'Sobrepeso';
+    } else if ( imc <= 34.9 ) {
+        return 'Obesidade classe I';
+    } else if ( imc <= 39.9 ) {
+        return 'Obesidade classe II';
+    } else {
+        return 'Obesidade classe III';
+    };
+};
