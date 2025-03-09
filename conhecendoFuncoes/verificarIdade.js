c2d07e47b73975d785c861ac25bfedb336072931
Bruno Daniel <bdao.profissional@gmail.com>
2025-03-08 22:26:04 -0300

    invocando funcao dentro de outra funcao

--- conhecendoFuncoes/verificarIdade.js
+++ conhecendoFuncoes/verificarIdade.js
@@ -1,9 +1,13 @@
 function verificarIdade( idade ) {
     if ( idade >= 18 ) {
-        console.log('É maior de idade');
+        console.log(`${informarNome('Bruno Daniel')} e sou maior de idade`);
     } else {
-        console.log('É menor de idade');
+        console.log(`${informarNome('Perry o Gato')} e sou menor de idade`);
     };
 };
-verificarIdade(30); // É maior de idade
-verificarIdade(8); // É menor de idade
+verificarIdade(30); // Meu nome é Bruno Daniel e sou maior de idade
+verificarIdade(8); // Meu nome é Perry o Gato e sou menor de idade
+
+function informarNome( nome ) {
+    return `Meu nome é ${nome}`;
+};
