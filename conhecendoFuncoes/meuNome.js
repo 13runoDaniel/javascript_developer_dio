e813d539d4cd8bbbb5e4095feb4b8397251503ca
Bruno Daniel <bdao.profissional@gmail.com>
2025-03-08 22:22:19 -0300

    funcao escrever nome

---
+++ conhecendoFuncoes/meuNome.js
@@ -0,0 +1,5 @@
+function informarNome( nome ) {
+    console.log(`Meu nome é ${nome}`);
+};
+informarNome('Bruno Daniel'); // Meu nome é Bruno Daniel
+informarNome('Perry o Gato'); // Meu nome é Perry o Gato
